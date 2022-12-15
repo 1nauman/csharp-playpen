ae1523cfe76714fd6932f39f03fcde7ddc3d84e2
Numan Mohammed <numan@qapita.com>
2022-12-15 16:56:33 +0530

    es-scratch write and read events sample

--- src/es-scratch/Program.cs
+++ src/es-scratch/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Json;
+using EventStore.Client;
 
-Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+
+//Console.WriteLine("Hello, World!");
+
+var client = new EventStoreClient(EventStoreClientSettings.Create("esdb://localhost:2113"));
+
+// var evt = new TestEvent { Data = "Some string info to persist" };
+//
+// var evtData = new EventData(Uuid.NewUuid(), nameof(TestEvent), JsonSerializer.SerializeToUtf8Bytes(evt));
+//
+// await client.AppendToStreamAsync(nameof(TestEvent), StreamState.Any, new[] { evtData });
+//
+// Console.WriteLine("Written event");
+
+var result = client.ReadStreamAsync(Direction.Forwards, nameof(TestEvent), StreamPosition.Start);
+
+var results = await result.ToListAsync();
+
+foreach (var item in results)
+{
+    var data = JsonSerializer.Deserialize<TestEvent>(item.Event.Data.Span);
+    Console.WriteLine($"{data.Id}---{data.Data}");
+}
+
+//Console.ReadKey();
+
+public class TestEvent
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString("D");
+    public string? Data { get; set; }
+}
