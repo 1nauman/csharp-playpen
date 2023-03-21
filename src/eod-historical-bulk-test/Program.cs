18d4c42c5acaafcac052bad547d73876d2328c70
Numan Mohammed <numan@qapita.com>
2023-03-22 02:00:13 +0530

    added EOD historical bulk sample post bug fix

---
+++ src/eod-historical-bulk-test/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Newtonsoft.Json;
+
+var eodApi = new EOD.API("demo");
+var result = eodApi.GetBulksAsync("US", null, null, "MSFT,INFY.NSE,TCS.NSE");
+
+Console.WriteLine(JsonConvert.SerializeObject(result.Result, Formatting.Indented,
+    new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore }));
