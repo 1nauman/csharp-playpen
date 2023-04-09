a91cf7df3e385710b053771622e2537515cb166f
Numan Mohammed <numan@qapita.com>
2023-04-09 15:20:44 +0530

    recaptcha enterprise project added

---
+++ src/google-recaptcha-enterprise/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace google_recaptcha_enterprise.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
