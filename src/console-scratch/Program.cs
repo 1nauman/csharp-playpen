f0d8323b1e3dd9a89a94e63ad7f97f4fca62107a
Numan Mohammed <numan@qapita.com>
2023-09-28 00:46:58 +0530

    Add UTC DateTime converter function
    Introduced a new function `GetDateTimeInUtc` to handle conversion of date strings to UTC formatted dates. This change is necessary to normalize date data received in different formats and time zones. The function also validates the input date string and throws an exception with detailed error if the input format is not `yyyy-MM-dd`. Also, commenting out a line invoking `Sha512()` method on a string, as it's currently not in use.

--- src/console-scratch/Program.cs
+++ src/console-scratch/Program.cs
@@ -4,6 +4,30 @@
 //     "1, 2 ,, ,abc ,9847 ,bnd".Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
 // Console.WriteLine(string.Join('|', cleanedString));
 
+using System.Globalization;
+using System.Runtime.CompilerServices;
 using console_scratch;
 
-"Test".Sha512();
+//"Test".Sha512();
+
+GetDateTimeInUtc("2021-09-10", "DateOfBirth");
+
+static string GetDateTimeInUtc(string dateString,
+    [CallerArgumentExpression("dateString")]
+    string memberName = "")
+{
+    if (!DateTime.TryParseExact(dateString, "yyyy-MM-dd", CultureInfo.InvariantCulture,
+            DateTimeStyles.AdjustToUniversal, out var result))
+    {
+        return result.ToString("yyyy-MM-ddTHH:mm:ss.000Z");
+    }
+
+    if (string.IsNullOrWhiteSpace(memberName))
+    {
+        throw new Exception($"Invalid value for date: '{dateString}'");
+    }
+
+    var segments = memberName.Split('.');
+    throw new Exception(
+        $"Invalid value received for '{segments.Last()}': '{dateString}'");
+}
