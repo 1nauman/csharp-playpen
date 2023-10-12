503769abe28559dc728c081e27f3ad1fc100cedd
Numan Mohammed <numan@qapita.com>
2023-10-13 01:51:13 +0530

    Refactor API interaction in niit-api-client
    Removed unused imports, commented lines and replaced with working code for interacting with the external API. This change was necessary to improve the performance, clean the codebase and to have a working interaction with the external API. Now, this updated code posts an HTTP request to the URL, fetches the data, decodes the JSON token, and displays the data in console output.

--- src/niit-api-client/Program.cs
+++ src/niit-api-client/Program.cs
@@ -1,63 +1,58 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Globalization;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
 using niit_api_client;
 
-// const string url = "https://dv5api.iniitian.com/api/ESOPExt/GetStaffData?Flag={0}";
-//
-// var services = new ServiceCollection();
-// services.AddHttpClient();
-// var serviceProvider = services.BuildServiceProvider();
-// var httpClient = serviceProvider.GetRequiredService<IHttpClientFactory>().CreateClient();
-// var token = GetAuthToken();
-// var targetUrl = string.Format(url, token);
-// var result = await httpClient.PostAsync(targetUrl, new StringContent(string.Empty));
-// Console.WriteLine(await result.Content.ReadAsStringAsync());
-// await serviceProvider.DisposeAsync();
+const string url = "https://dv5api.iniitian.com/api/ESOPExt/GetStaffData?Flag={0}";
+const string key = ""; // Guess this is the signing key for JWT
 
-var dateTime = new DateTime(2023, 9, 10);
+var services = new ServiceCollection();
+services.AddHttpClient();
+var serviceProvider = services.BuildServiceProvider();
+var httpClient = serviceProvider.GetRequiredService<IHttpClientFactory>().CreateClient();
+var token = GetAuthToken(DateTime.Now);
+//Console.WriteLine(token);
+var targetUrl = string.Format(url, token);
 
-Console.WriteLine(GetAuthToken(dateTime));
-Console.WriteLine(BuildToken(dateTime));
+var response = await httpClient.PostAsync(targetUrl, new StringContent(string.Empty));
+var resultString = await response.Content.ReadAsStringAsync();
+//Console.WriteLine(resultString);
+
+try
+{
+    if (string.IsNullOrWhiteSpace(resultString))
+    {
+        Console.WriteLine("Empty result string");
+        return;
+    }
+
+    if (resultString == "\"\"")
+    {
+        Console.WriteLine("Empty \"\" result string");
+        return;
+    }
+    
+    var resultJson = JsonWebToken.Decode(resultString, key);
+    Console.WriteLine(resultJson);
+}
+finally
+{
+    await serviceProvider.DisposeAsync();
+}
 
 static string GetAuthToken(DateTime dateTime)
 {
-    var dateTimeFormat = "MM/dd/yyyy hh:mm:ss tt";
+    const string dateTimeFormat = "MM/dd/yyyy hh:mm:ss tt";
 
     var dateCurrent = dateTime.ToString(dateTimeFormat, CultureInfo.InvariantCulture);
 
     var payLoad = new Dictionary<string, string>
     {
         { "CID", "TalentRepository" },
-
-        { "Time", dateCurrent }
+        { "Time", dateCurrent }, // Time at which the token is generated
+        { "_Date", "20 Mar 2023" } // Date for which we want to get the data for
     };
 
-    return JsonWebToken.Encode(payLoad, "superSecretKeyWhichIsMuchLongerThanBefore@345", JwtHashAlgorithm.HS256);
-}
-
-static string BuildToken(DateTime dateTime)
-{
-    var signingKey = "superSecretKeyWhichIsMuchLongerThanBefore@345";
-    
-    var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey));
-    var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-
-    var descriptor = new SecurityTokenDescriptor
-    {
-        Claims = new Dictionary<string, object>
-        {
-            { "CID", "TalentRepository" },
-            { "Time", dateTime.ToString("MM/dd/yyyy hh:mm:ss tt") }
-        },
-        SigningCredentials = credentials
-    };
-    
-    var tokenHandler = new JwtSecurityTokenHandler();
-    return tokenHandler.WriteToken(tokenHandler.CreateJwtSecurityToken(descriptor));
+    return JsonWebToken.Encode(payLoad, key, JwtHashAlgorithm.HS256);
 }
