6f44d74f36b561ba20a4867a9eb28fb739e2948f
Numan Mohammed <numan@qapita.com>
2024-01-17 22:12:25 +0530

    ncalc-sample

---
+++ src/ncalc-playpen/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using NCalc;
+
+var settlementAmountExpr = new Expression("Round([SettlementPrice] * [Units], 2)")
+{
+    Parameters =
+    {
+        ["SettlementPrice"] = new Expression("[FMV]"),
+        ["Units"] = 10
+    }
+};
+
+settlementAmountExpr.EvaluateParameter += (name, args) =>
+{
+    if (name == "FMV")
+    {
+        args.Result = 123.45d;
+    }
+};
+
+var settlementAmount = settlementAmountExpr.Evaluate();
+
+Console.WriteLine("Settlement Amount: {0}", settlementAmount);
