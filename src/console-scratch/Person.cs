6c8b3463b07214e1f53b86e56e759b2ad6ddac91
Numan Mohammed <numan@qapita.com>
2024-02-25 11:21:51 +0530

    `in` sample on ref type

---
+++ src/console-scratch/Person.cs
@@ -0,0 +1,8 @@
+namespace console_scratch;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+}
--- src/console-scratch/Program.cs
+++ src/console-scratch/Program.cs
@@ -8,26 +8,43 @@ using System.Globalization;
 using System.Runtime.CompilerServices;
 using console_scratch;
 
+var p1 = new Person
+{
+    Name = "John Doe",
+    Age = 42,
+    Gender = "M"
+};
+
+PrintPerson(p1);
+
+void PrintPerson(in Person person)
+{
+    person.Name = "Jane Doe";
+    Console.WriteLine($"Name: {person.Name}");
+    Console.WriteLine($"Age: {person.Age}");
+    Console.WriteLine($"Gender: {person.Gender}");
+}
+
 //"Test".Sha512();
 
-GetDateTimeInUtc("2021-09-10", "DateOfBirth");
-
-static string GetDateTimeInUtc(string dateString,
-    [CallerArgumentExpression("dateString")]
-    string memberName = "")
-{
-    if (!DateTime.TryParseExact(dateString, "yyyy-MM-dd", CultureInfo.InvariantCulture,
-            DateTimeStyles.AdjustToUniversal, out var result))
-    {
-        return result.ToString("yyyy-MM-ddTHH:mm:ss.000Z");
-    }
-
-    if (string.IsNullOrWhiteSpace(memberName))
-    {
-        throw new Exception($"Invalid value for date: '{dateString}'");
-    }
-
-    var segments = memberName.Split('.');
-    throw new Exception(
-        $"Invalid value received for '{segments.Last()}': '{dateString}'");
-}
+// GetDateTimeInUtc("2021-09-10", "DateOfBirth");
+//
+// static string GetDateTimeInUtc(string dateString,
+//     [CallerArgumentExpression("dateString")]
+//     string memberName = "")
+// {
+//     if (!DateTime.TryParseExact(dateString, "yyyy-MM-dd", CultureInfo.InvariantCulture,
+//             DateTimeStyles.AdjustToUniversal, out var result))
+//     {
+//         return result.ToString("yyyy-MM-ddTHH:mm:ss.000Z");
+//     }
+//
+//     if (string.IsNullOrWhiteSpace(memberName))
+//     {
+//         throw new Exception($"Invalid value for date: '{dateString}'");
+//     }
+//
+//     var segments = memberName.Split('.');
+//     throw new Exception(
+//         $"Invalid value received for '{segments.Last()}': '{dateString}'");
+// }
