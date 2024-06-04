891afe578ca7876f76d6e168248f130249471db4
Numan Mohammed <numan@qapita.com>
2024-06-04 13:46:42 +0530

    initial project setup for audit.net

---
+++ src/audit.net-mongo-client/Person.cs
@@ -0,0 +1,8 @@
+namespace audit.net_mongo_client;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    
+    public string LastName { get; set; }
+}
---
+++ src/audit.net-mongo-client/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Audit.MongoClient;
+using audit.net_mongo_client;
+using MongoDB.Driver;
+
+var mongoClientSettings = new MongoClientSettings
+{
+    Server = new MongoServerAddress("localhost", 27017),
+    ClusterConfigurator = clusterBuilder => clusterBuilder.AddAuditSubscriber(ac => ac.IncludeReply())
+};
+
+var _client = new MongoClient(mongoClientSettings);
+
+var db = _client.GetDatabase("audit_test");
+
+await db.GetCollection<Person>("persons").InsertOneAsync(new Person
+{
+    FirstName = "Numan",
+    LastName = "Mohammed"
+});
+
